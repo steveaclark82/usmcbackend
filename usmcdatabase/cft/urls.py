8f0f12803cf0419cedf1f70495949abd2461d14c
Steve Clark <steve.a.clark48@gmail.com>
2021-09-27 14:43:59 -0500

    updated urls

--- usmcdatabase/cft/urls.py
+++ usmcdatabase/cft/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('/', views.get_cft_score),
+    path('/all', views.get_cft_score),
     path('maneuver_fire/', views.get_cft_muf),
     path('ammo_lift/', views.get_cft_ammo),
     path('movement_contact/', views.get_cft_mtc),
