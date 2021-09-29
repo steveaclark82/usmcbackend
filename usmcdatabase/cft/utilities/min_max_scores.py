51410b3cc8d9d983613a6a0fb358ec09c01caa9f
Steve Clark <steve.a.clark48@gmail.com>
2021-09-29 11:25:55 -0500

    added score class

--- usmcdatabase/cft/utilities/min_max_scores.py
+++ usmcdatabase/cft/utilities/min_max_scores.py
@@ -13,4 +13,15 @@ def slower_than_min_time(age, gender, time, high_alt, event):
                                    & (event.score == 40)).first()
     return time > min_score.time
 
+def above_max_reps(age, gender, reps, event):
+    max_score = event.query.filter((event.age == age)
+                                   & (event.gender == gender)
+                                   & (event.score == max_points)).first()
+    return reps > max_score.reps
 
+def below_min_reps(age, gender, reps, event):
+        min_allowed = 60 if gender == "F" else 40
+        min_score = event.query.filter((event.age == age)
+                                         & (event.gender == gender)
+                                         & (event.score == min_allowed)).first()
+        return reps < min_score.reps
