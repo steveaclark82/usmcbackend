3b990c0549b52d995194aaff59520e58e81c6d02
Steve Clark <steve.a.clark48@gmail.com>
2021-09-30 00:40:00 -0500

    updated urls

--- usmcdatabase/cft/urls.py
+++ usmcdatabase/cft/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('all', views.cft),
+    path('', views.cft),
 ]
--- usmcdatabase/pft/urls.py
+++ usmcdatabase/pft/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
-    path('all', views.pft),
+    path('', views.pft),
  
 ]
