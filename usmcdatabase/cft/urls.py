a9706a225f632c6b1b4ccf598485979ccc4184dd
Steve Clark <steve.a.clark48@gmail.com>
2021-10-01 03:14:00 -0500

    updated urls

--- usmcdatabase/cft/urls.py
+++ usmcdatabase/cft/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.cft),
+    path('<int:id>/', views.get_Movement_Contact),
+    path('<int:id>/', views.get_Maneuver_Fire),
+    path('<int:id>/', views.get_Ammo_lift),
 ]
