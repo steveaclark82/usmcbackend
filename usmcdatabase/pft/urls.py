d8e0366727ac590239385ceb3564ddb8cdd78272
Steve Clark <steve.a.clark48@gmail.com>
2021-10-01 03:34:27 -0500

    updated urls

--- usmcdatabase/pft/urls.py
+++ usmcdatabase/pft/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.pft),
+    path('<int:id>/', views.get_Three_Mile),
+    path('<int:id>/', views.get_Crunches),
+    path('<int:id>/', views.get_Plank),
+    path('<int:id>/', views.get_Row),
+    path('<int:id>/', views.get_Pullups),
+    path('<int:id>/', views.get_Pushups),
  
 ]
